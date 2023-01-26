ecbc7a755a660e3bdfeaa4767b5123d24c2e6f1a
Andor Fehér <andor.feher@hotjar.com>
2023-01-26 13:08:40 +0100

    Fix turnoff

--- turnoff.js
+++ turnoff.js
@@ -2,27 +2,19 @@
 const { lgtv } = require('./index.js')
 const EventNames = require('./helpers/EventNames.js');
 
-function switchToInput( input ){
-    if( !input ){
-        throw new Error('No input specified.')
-    }
-
-    const callback = function () {
-        const promise = lgtv.turnOff();
-        try {
-            promise.then(()=>{
-                lgtv.disconnect();
-                process.exit()
-            })
-        } catch (err) {
+const callback = function () {
+    const promise = lgtv.turnOff();
+    try {
+        promise.then(()=>{
             lgtv.disconnect();
             process.exit()
-        }
+        })
+    } catch (err) {
+        lgtv.disconnect();
+        process.exit()
     }
-
-    lgtv.on(EventNames.TV_CONNECTED, callback);
-
-    lgtv.connect()
 }
 
-module.exports = switchToInput
+lgtv.on(EventNames.TV_CONNECTED, callback);
+
+lgtv.connect()
